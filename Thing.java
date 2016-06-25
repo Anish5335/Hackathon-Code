b82e56ea48e348008d38f9cd59a0af07e29be856
Anish5335 <anishgsundar@gmail.com>
2016-06-24 21:39:20 -0700

    Create Thing.java

---
+++ Thing.java
@@ -0,0 +1,20 @@
+package Hackathon;
+
+import Hackathon.Thing;
+
+public interface Thing {
+
+	public void draw();
+	public void move();
+	public boolean isTouching(Thing t);
+	public boolean isEnemy();
+	public int getX();
+	public int getY();
+	public void setX(int x);
+	public void setY(int y);
+	public int getSize();
+	public boolean shoot();
+	public boolean outside();
+	public boolean isEnemySpaceship();
+	
+}
